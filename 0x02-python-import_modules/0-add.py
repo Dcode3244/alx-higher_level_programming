1d96307238939fe5c5cee4445d23e2043a82602b
Dcode3244 <dagimourgie111@gmail.com>
2022-09-22 12:37:45 +0300

    modified task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-import add_0
+from add_0 import add
 a, b = 1, 2
 if __name__ == '__main__':
-    print(a, " + ", b, " = ", add_0.add(a, b))
+    print(a, " + ", b, " = ", add(a, b))
