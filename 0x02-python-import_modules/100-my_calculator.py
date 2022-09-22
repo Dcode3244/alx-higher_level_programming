4f884f2443d4119745422bcf35b8e5b13419ace7
Dcode3244 <dagimourgie111@gmail.com>
2022-09-22 14:34:08 +0300

    fixed style error

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
         print("{} {} {} = {}".format(a, operator, b, add(a, b)))
     elif operator == '-':
         print("{} {} {} = {}".format(a, operator, b, sub(a, b)))
-    elif operator == '\*':
+    elif operator == '*':
         print("{} {} {} = {}".format(a, operator, b, mul(a, b)))
     else:
         print("{} {} {} = {}".format(a, operator, b, div(a, b)))
