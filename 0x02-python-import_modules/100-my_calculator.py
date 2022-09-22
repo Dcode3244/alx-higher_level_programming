2e08a848ba4aa7b9c96819e387eba1e6b857e585
Dcode3244 <dagimourgie111@gmail.com>
2022-09-22 14:29:05 +0300

    added advanced task 6

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    import sys
+    from calculator_1 import add, sub, mul, div
+    if len(sys.argv) != 4:
+        print("Usage: .100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    operator = sys.argv[2]
+    if operator != '+' and operator != '-' and\
+            operator != '*' and operator != '/':
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+
+    if operator == '+':
+        print("{} {} {} = {}".format(a, operator, b, add(a, b)))
+    elif operator == '-':
+        print("{} {} {} = {}".format(a, operator, b, sub(a, b)))
+    elif operator == '*':
+        print("{} {} {} = {}".format(a, operator, b, mul(a, b)))
+    else:
+        print("{} {} {} = {}".format(a, operator, b, div(a, b)))
