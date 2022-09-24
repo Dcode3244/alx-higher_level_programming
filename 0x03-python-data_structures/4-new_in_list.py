3c2ad992526cfffb672e25daaa08bacb15b112d9
Dcode3244 <dagimourgie111@gmail.com>
2022-09-24 15:51:16 +0300

    bug fix

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx > (len(my_list) - 1):
-        return None
+        return my_list
     new_list = []
     for i in range(len(my_list)):
         new_list.append(my_list[i])
