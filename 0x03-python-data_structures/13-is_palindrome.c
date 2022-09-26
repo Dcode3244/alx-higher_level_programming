4cd39cdf46c066cecd2e3801142a6483dbf1a3f6
Dcode3244 <dagimourgie111@gmail.com>
2022-09-26 12:56:27 +0300

    efficiency fix

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -31,24 +31,43 @@ listint_t *rev(listint_t **head)
  */
 int is_palindrome(listint_t **head)
 {
-	int i, nums[60];
-	listint_t *ptr;
+	int i;
+	listint_t *ptr, *new, *new_ptr;
 
 	ptr = rev(head);
-
+	new = malloc(sizeof(listint_t));
+	new_ptr = new;
 	for (i = 0; *head != NULL; i++)
 	{
-		nums[i] = (*head)->n;
+		new->n = (*head)->n;
 		*head = (*head)->next;
+		if (*head != NULL)
+		{
+			new->next = malloc(sizeof(listint_t));
+			new = new->next;
+		}
+		else
+			new->next = NULL;
 	}
 	*head = ptr;
 	rev(head);
 
-	for (i = 0; *head != NULL; i++)
+	new = new_ptr;
+	while (*head != NULL)
 	{
-		if ((*head)->n != nums[i])
+		if ((*head)->n != new_ptr->n)
 			return (0);
 		*head = (*head)->next;
+		new_ptr = new_ptr->next;
 	}
+
+	new_ptr = new;
+	while (new != NULL)
+	{
+		new_ptr = new_ptr->next;
+		free(new);
+		new = new_ptr;
+	}
+
 	return (1);
 }
