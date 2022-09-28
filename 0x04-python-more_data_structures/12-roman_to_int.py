22b13eb80c675dd841d7fa8b3b93ff145cd002f5
Dcode3244 <dagimourgie111@gmail.com>
2022-09-28 20:57:19 +0300

    modification

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
-        return 0
+        return 1
     converted = []
     final = []
     roman = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
