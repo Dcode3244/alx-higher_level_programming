dd74c265e44f09bc866a328155d39d7211113d24
Dcode3244 <dagimourgie111@gmail.com>
2022-09-28 22:52:41 +0300

    bug fix

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,65 @@
+#include <python.c>
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+
+/**
+ * print_python_list - prints basic info about python lists
+ * @p: pointer to the python list
+*/
+void print_python_list(PyObject *p)
+{
+	PyListObject *Obj_list = (PyListObject *)p;
+	PyVarObject *Obj_var = (PyVarObject *)p;
+	int size, allocated, i;
+	const char *type;
+
+
+	allocated = Obj_list->allocated;
+	size = Obj_var->ob_size;
+
+	printf("[*] Python list info\n[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %d\n", allocated);
+
+	for (i = 0; i < size; i++)
+	{
+		type = Obj_list->ob_item[i]->ob_type->tp_name;
+		printf("Element %d: %s\n", i, type);
+		if (strcmp(type, "bytes") == 0)
+			print_python_bytes(Obj_list->ob_item[i]);
+	}
+}
+
+/**
+ * print_python_bytes - Prints basic info about Python byte objects
+ * @p: A PyObject byte object.
+ */
+void print_python_bytes(PyObject *p)
+{
+	PyBytesObject *bytes = (PyBytesObject *)p;
+	unsigned char i, size;
+
+	printf("[.] bytes object info\n");
+	if (strcmp(p->ob_type->tp_name, "bytes") != 0)
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+		return;
+	}
+
+	printf("  size: %ld\n", ((PyVarObject *)p)->ob_size);
+	printf("  trying string: %s\n", bytes->ob_sval);
+
+	if (((PyVarObject *)p)->ob_size > 10)
+		size = 10;
+	else
+		size = ((PyVarObject *)p)->ob_size + 1;
+
+	printf("  first %d bytes: ", size);
+	for (i = 0; i < size; i++)
+	{
+		printf("%02hhx", bytes->ob_sval[i]);
+		if (i == (size - 1))
+			printf("\n");
+		else
+			printf(" ");
+	}
+}
