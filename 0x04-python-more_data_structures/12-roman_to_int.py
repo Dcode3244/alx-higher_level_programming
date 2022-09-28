23036dd6f3c3c76eaf4336961aed20993c7c2006
Dcode3244 <dagimourgie111@gmail.com>
2022-09-28 21:17:37 +0300

    cleanup

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -10,31 +10,10 @@ def roman_to_int(roman_string):
         for rom in list(roman.keys()):
             if char == rom:
                 converted.append(roman[char])
-    num = converted[0]
-    count = 1
-    # if the converted list lenght is 1 return the value
-    if len(converted) == 1:
-        return converted[0]
-    """if the converted list have continuous duplicate values,
-    add them and remove the duplicate value"""
-    for i in range(1, len(converted)):
-        if num == converted[i]:
-            count += 1
-            if i + 1 == len(converted):
-                final.append(num * count)
-                break
-            continue
-        final.append(num * count)
-        num = converted[i]
-        count = 1
-        if i + 1 == len(converted):
-            final. append(num)
-
-    # add the list values to find the final converted number
-    for i in range(len(final) - 1):
-        if (final[i] < final[i + 1]):
-            final[i] = -1 * final[i]
-    ans = 0
-    for i in final:
-        ans += i
-    return ans
+    num = 0
+    for i in range(len(converted)):
+        if i != (len(converted) - 1) and converted[i] < converted[i + 1]:
+            num += converted[i] * -1
+        else:
+            num += converted[i]
+    return num
