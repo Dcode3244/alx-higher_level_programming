5687f0d803db5c89f34cb6f28bf4ca002237ce07
Dcode3244 <dagimourgie111@gmail.com>
2022-10-10 13:14:26 +0300

    requirement fix

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -15,4 +15,6 @@ def list_division(my_list_1, my_list_2, list_length):
             except TypeError:
                 print("wrong type")
                 new_list.append(0)
+            finally:
+                pass
         return new_list
