224e6ce709c85652f0d0b0ed7a25343aa0e8f9a4
Dcode3244 <dagimourgie111@gmail.com>
2022-10-10 12:25:49 +0300

    quick fix;

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,6 +8,6 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end="")
         print()
         return list_len
-    except IndexError:
+    except (IndexError, TypeError):
         print()
         return list_len - 1
