475afbb16a6df3b89aaae5c2ea1e199f641be791
Dcode3244 <dagimourgie111@gmail.com>
2022-10-10 21:17:30 +0300

    quick fix;

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    ret = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception("Too far")
+            else:
+                ret = ret + (a ** b / i)
+        except:
+            ret = b + a
+            break
+    return ret
