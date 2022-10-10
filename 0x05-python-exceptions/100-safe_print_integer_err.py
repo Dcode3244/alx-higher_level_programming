600ac5686bb5cabdcae8f3b5983da506865726b7
Dcode3244 <dagimourgie111@gmail.com>
2022-10-10 13:36:06 +0300

    quick fix;

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+import sys
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
 
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except (ValueError, TypeError) as e:
-        print("Exception: {}".format(e))
+        eprint("Exception: {}".format(e))
         return False
     else:
         return (True)
