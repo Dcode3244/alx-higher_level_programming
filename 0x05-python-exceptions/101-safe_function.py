4015912ce03f5f2e26dcdf9d7c60070edc284ee3
Dcode3244 <dagimourgie111@gmail.com>
2022-10-10 13:58:03 +0300

    added advanced task 101

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        return fct(args[0], args[1])
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return None
