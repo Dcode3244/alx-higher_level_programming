67740f1e3ca11756504fce1b142ef2dec16c6ea1
Dcode3244 <dagimourgie111@gmail.com>
2022-10-11 20:04:44 +0300

    bug fix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -11,8 +11,8 @@ class Square:
         Args:
             size (int): size of a square
         """
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
