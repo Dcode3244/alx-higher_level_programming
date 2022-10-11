11148119923a9d0d047f93f5a2cf09b4bf757639
Dcode3244 <dagimourgie111@gmail.com>
2022-10-11 19:50:51 +0300

    bug fix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -42,14 +42,23 @@ class Square:
 
         return self.__position
 
-    @position.setter
+    """@position.setter
     def position(self, value):
-        """ position setter """
+         position setter
 
         if (not isinstance(value, tuple) or
                 len(value) != 2 or
                 not all(isinstance(val, int) for val in value) or
                 not all(val >= 0 for val in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value"""
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
@@ -62,7 +71,7 @@ class Square:
         return (self.__size) ** 2
 
     def my_print(self):
-        """ prints a square
+        """ prints a square """
         if self.__size == 0:
             print()
         else:
@@ -73,13 +82,4 @@ class Square:
                     print(" ", end="")
                 for j in range(self.__size):
                     print("#", end="")
-                print() """
-        if self.__size == 0:
-            print("")
-            return
-
-        [print("") for i in range(0, self.__position[1])]
-        for i in range(0, self.__size):
-            [print(" ", end="") for j in range(0, self.__position[0])]
-            [print("#", end="") for k in range(0, self.__size)]
-            print("")
+                print()
