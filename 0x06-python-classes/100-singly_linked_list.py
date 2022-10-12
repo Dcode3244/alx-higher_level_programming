2c599e1a2e66174465e3b6e2e1a963a08a7a2f62
Dcode3244 <dagimourgie111@gmail.com>
2022-10-12 19:56:17 +0300

    requirement fix

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -3,26 +3,39 @@
 class Node:
     """ defines a node of singly linked list """
     def __init__(self, data, next_node=None):
-        """ initializes the node """
+        """ initializes the node
+
+            Args:
+                data (int): number inside a node
+                node (node): a node
+        """
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
+        """ data getter """
+
         return self.__data
 
     @data.setter
     def data(self, value):
+        """ data setter """
+
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
         self.__data = value
 
     @property
     def next_node(self):
+        """ next_node getter """
+
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
+        """ next_node setter """
+
         if not isinstance(value, Node) and value is not None:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
@@ -30,6 +43,8 @@ class Node:
 
 class SinglyLinkedList:
     def __init__(self):
+        """ initializes a singly linked list node """
+
         self.__head = None
 
     def sorted_insert(self, value):
@@ -59,6 +74,7 @@ class SinglyLinkedList:
 
     def __str__(self):
         """ defines print() value of SinglyLinkedList """
+
         temp = self.__head
         while temp.next_node is not None:
             print(str(temp.data))
