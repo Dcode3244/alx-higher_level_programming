66f404c52dc65aed2096d2d5cb7fd9cb39a800c5
Dcode3244 <dagimourgie111@gmail.com>
2022-10-17 19:37:10 +0300

    added task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+a module containing simple class Rectangle
+that defines a rectangle
+
+"""
+
+
+class Rectangle:
+    """
+    a simple class that defines a rectangle
+
+    """
+    pass
