9f8034cdf718c2c55d2808e3db3af1ebdaf63b3e
Dcode3244 <dagimourgie111@gmail.com>
2022-10-24 15:34:29 +0300

    task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" defines a square class that inherits from BaseGeometry """
+
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Square(BaseGeometry):
+    """ a square class that inherits from BaseGeometry class """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        return ("[Rectangle] {}/{}".format(self.__size, self.__size))
