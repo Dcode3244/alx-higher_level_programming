e7fdfbacded79fe7b392ecd7678ce15f0cc42557
Dcode3244 <dagimourgie111@gmail.com>
2022-10-24 20:15:26 +0300

    task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" defines a square class that inherits from BaseGeometry """
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """ a square class that inherits from BaseGeometry class """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        return ("[square] {}/{}".format(self.__size, self.__size))
