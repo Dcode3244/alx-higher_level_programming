69c7ba0f4d859b5b75be8b1cfc3f18ec8bbc69da
Dcode3244 <dagimourgie111@gmail.com>
2022-10-25 22:41:34 +0300

    task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" defines a class Student that defines a student """
+
+
+class Student:
+    """ a class that defines a student """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if (isinstance(attrs, list) and
+                all(isinstance(s, str) for s in attrs)):
+            new = dict()
+            for attr in attrs:
+                if attr in self.__dict__:
+                    new[attr] = (self.__dict__)[attr]
+            return new
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """ replaces all atributes of the student instance """
+        self.first_name = json["first_name"]
+        self.last_name = json["last_name"]
+        self.age = json["age"]
