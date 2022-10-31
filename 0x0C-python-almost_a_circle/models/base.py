ae0cb34bd6a834a5304d6e4867246ebf1204ab38
Dcode3244 <dagimourgie111@gmail.com>
2022-10-31 18:26:29 +0300

    fixed to_json_string() function"

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,6 +18,8 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """ returns json representation of list_dictionaries """
+        if list_dictionaries is None or list_dictionaries = []:
+            return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
