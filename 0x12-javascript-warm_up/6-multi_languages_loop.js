67d2947e5a3880621602a8488541a87a9a1abaaf
Dcode3244 <Dagimourgie111@gmail.com>
2022-12-19 13:11:14 +0300

    typo fix

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const languages = ['C is fun', 'Python3 is cool', 'JavaScript is amazing'];
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < 3; i++) {
   console.log(languages[i]);
