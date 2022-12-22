0363cf3367aa17da44f076e12271a0af1dc29321
Dcode3244 <dagimourgie111@gmail.com>
2022-12-22 18:51:05 +0000

    bug fix

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-""" lists all states with a name starting with N from
-the database hbtn_0e_0_usa
+""" takes in arguments and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument.
+But this time, write one that is safe from MySQL injections!
 """
 import sys
 import MySQLdb
@@ -10,7 +11,7 @@ if __name__ == "__main__":
     uname, passwd, db, search = args[1], args[2], args[3], args[4]
     db = MySQLdb.connect(host="localhost", user=uname, passwd=passwd, db=db)
     cur = db.cursor()
-    cur.execute('SELECT * FROM states WHERE name LIKE BINARY "{}"\
-                ORDER BY id'.format(search))
-    for s in cur.fetchall():
-        print(s)
+    cur.execute('SELECT * FROM states')
+    for state in cur.fetchall():
+        if (search == state[1]):
+            print(state)
