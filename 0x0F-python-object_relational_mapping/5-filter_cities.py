cc258b3730955451df9f2cf94283ac12c9105d81
Dcode3244 <dagimourgie111@gmail.com>
2022-12-22 19:24:49 +0000

    task 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+Displays all cities of a given state from the states table
+of the database hbtn_0e_4_usa Safe from SQL injections.
+'''
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    name, passwd, db, search = argv[1], argv[2], argv[3], argv[4]
+    db = MySQLdb.connect(user=name, passwd=passwd, db=db)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM `cities` as `c` \
+            INNER JOIN `states` as `s` \
+            ON `c`.`state_id` = `s`.`id` \
+            ORDER BY `c`.`id`")
+    print(", ".join([ct[2] for ct in cur.fetchall() if ct[4] == sys.argv[4]]))
