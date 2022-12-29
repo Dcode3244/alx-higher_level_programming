9c0d4bb5e898df0532e2a0680c686d54bdff4601
Dcode3244 <Dagimourgie111@gmail.com>
2022-12-29 13:08:32 +0300

    task 6 final

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,11 +5,7 @@ const Sqr = require('./5-square');
 module.exports = class Square extends Sqr {
   charPrint (c = 'X') {
     for (let i = 0; i < this.height; i++) {
-      const rows = [];
-      for (let j = 0; j < this.width; j++) {
-        rows.push(c);
-      }
-      console.log(rows.join(''));
+      console.log(c.repeat(this.width));
     }
   }
 };
