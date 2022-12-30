f107426a3ff49033a1dd9adea6a2f3d16290942e
Dcode3244 <Dagimourgie111@gmail.com>
2022-12-30 13:57:17 +0300

    removed some files

--- 0x13-javascript_objects_scopes_closures/pushed/7-occurrences.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-
-exports.nbOccurences = function (list, searchElement) {
-  function counter (acc, curr) {
-    return (curr === searchElement ? acc += 1 : acc);
-  }
-  return (list.reduce(counter, 0));
-};
