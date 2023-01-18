7f193ddeb5d1d3fcbbb036f3e514877788446213
dcode3244 <dagimourgie111@gmail.com>
2023-01-17 17:16:14 -0800

    advancrd task

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+''' takes 2 arguments in order to solve this challenge.
+
+The first argument will be the repository name
+The second argument will be the owner name
+You must use the packages requests and sys
+You are not allowed to import packages other than requests and sys
+You don’t need to check arguments passed to the script (number or type)
+'''
+
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
