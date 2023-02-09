a2e616fbf6c3f16cef39c2c1b31c4bb4ea502ca6
dcode3244 <dagimourgie111@gmail.com>
2023-02-09 13:16:44 +0000

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,10 +1,19 @@
 #!/usr/bin/node
 
 const request = require('request');
+let count = 0;
 
 const callback = (err, response, body) => {
   if (err) { return console.error(err); }
-  console.log(JSON.parse(body).count);
+  const results = JSON.parse(body).results;
+  for (const result of results) {
+    for (const character of result.characters) {
+      if (character.includes('/people/18/')) {
+        count++;
+      }
+    }
+  }
+  console.log(count);
 };
 
 request(process.argv[2], callback);
