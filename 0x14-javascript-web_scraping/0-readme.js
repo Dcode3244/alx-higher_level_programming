1edabd601eb8915f7479e7cd22cacfc33f7cb0fa
dcode3244 <dagimourgie111@gmail.com>
2023-02-09 12:07:32 +0000

    task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (err) {
+    return console.error(err);
+  }
+  console.log(data);
+});
