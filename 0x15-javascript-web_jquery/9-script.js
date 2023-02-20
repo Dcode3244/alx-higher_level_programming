fd6f70ba1d843ff5d2cadde99fb333c1dceeda74
dcode3244 <dagimourgie111@gmail.com>
2023-02-20 18:21:33 +0300

    style fix

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,8 +1,5 @@
-// $('document').ready(function () {
-//   $.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data) {
-//     $('DIV#hello').text(data.hello);
-//   });
-// });
-$.getJSON("https://fourtonfish.com/hellosalut/?lang=fr", function (data) {
-  $("DIV#hello").html(data.hello)
+$('document').ready(function () {
+  $.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data) {
+    $('DIV#hello').text(data.hello);
+  });
 });
